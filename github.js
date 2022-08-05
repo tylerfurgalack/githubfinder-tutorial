8bbed8feb004989af70a017581f30e770fe95636
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-08-05 15:09:00 -0400

    fetch and display repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'c0b7de384c6ee72c5fc0'
     this.client_secret = 'fb734342d8f38e44bbad20152dc9774c07666452';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
